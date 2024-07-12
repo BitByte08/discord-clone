d5e27418d1931cb3dd21aa4be72528573395fb24
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-07-12 18:38:45 +0900

    edit code

--- script/index.js
+++ script/index.js
@@ -1,6 +1,7 @@
 let idx=0;
+let rastchatidx = "user0";
 
-
+console.log('반가워요!');
 function errorend(){
     document.getElementById("error").style.display='none';
 }
@@ -8,12 +9,12 @@ function showerror(){
     document.getElementById("error").style.display='block';
 }
 
+let DMlist = document.getElementsByClassName('rectangle-parent');
+let Onlinelist = document.getElementsByClassName('rectangle-div1');
+let Alllist = document.getElementsByClassName('allfriends');
 
 fetch('data.json').then((response)=>{return response.json()})
 .then((obj)=>{
-    let DMlist = document.getElementsByClassName('rectangle-parent');
-    let Onlinelist = document.getElementsByClassName('rectangle-div1');
-    let Alllist = document.getElementsByClassName('allfriends');
     obj.user.map((item)=>{
         idx++;
         console.log(item);
@@ -21,13 +22,13 @@ fetch('data.json').then((response)=>{return response.json()})
             DMlist[0].innerHTML+=`<div class="dm1" id="no">
                                     <div class="select1">
                                     </div>
-                                    <div class="name" id="${item['id']}">${item['name']}</div>
+                                    <div class="name" id="user${item['id']}">${item['name']}</div>
                                     <img class="user-frofile-img" alt="" src="${item['img']}">
                                     <img class="component-12-icon1" alt="" src="source/frofile_icon/Online_no.svg">
                                 </div>`;
             let listdiv=`<div class="onlineuser">
                                         <img class="user-frofile-img2" alt="" src="${item['img']}">
-                                        <img class="component-12-icon2" alt="" src="source/frofile_icon/Online_no.svg">
+                                        <img class="component-12-icon2" alt="" src="source/frofile_icon/Online_list.svg">
                                         <span>${item['name']}</span>
                                         <p>온라인</p>
                                     </div>`;
@@ -37,13 +38,13 @@ fetch('data.json').then((response)=>{return response.json()})
             DMlist[0].innerHTML+=`<div class="dm1" id="no">
                                     <div class="select1">
                                     </div>
-                                    <div class="name" id="${item['id']}">${item['name']}</div>
+                                    <div class="name" id="user${item['id']}">${item['name']}</div>
                                     <img class="user-frofile-img" alt="" src="${item['img']}">
                                     <img class="component-12-icon1" alt="" src="source/frofile_icon/Offline_no.svg">
                                 </div>`
             let listdiv2=`<div class="offlineuser">
                             <img class="user-frofile-img2" alt="" src="${item['img']}">
-                            <img class="component-12-icon2" alt="" src="source/frofile_icon/Offline_no.svg">
+                            <img class="component-12-icon2" alt="" src="source/frofile_icon/Offline_list.svg">
                             <span>${item['name']}</span>
                             <p>오프라인</p>
                         </div>`;
@@ -63,6 +64,8 @@ function all(){
     }
 }
 function selector(){
+    if(this.id==='yes') return;
+
     let html = this.innerHTML;
     html = html.replace('select1', 'select');
     html = html.replace('_no', '_on');
@@ -75,6 +78,37 @@ function selector(){
     html2 = html2.replace('select', 'select1');
     a.innerHTML=html2;
     a.id = 'no';
+
+    let main = document.getElementById('mainon');
+    let chat = document.getElementsByClassName('chat');
+    let main2 = document.getElementsByClassName('info');
+    if(this.parentNode.className!='dm-parent'){
+        main.setAttribute('id','mainoff');
+        chat[0].setAttribute('id','mainon');
+        dmreader();
+        return;
+    }
+    main.setAttribute('id','mainoff');
+    main2[0].setAttribute('id','mainon');
+    
+
+    dmreader();
+}
+function dmreader(){
+    let user = document.getElementById('yes');
+    if(user.parentNode.className==='rectangle-parent'){
+        let name = user.getElementsByClassName('name');
+        let id = name[0].id;
+        var chatdis = document.getElementsByClassName('chat');
+        console.log(chatdis[0]);
+        let newchat = chatdis[0].querySelector("#"+id);
+        let lastchat = chatdis[0].querySelector("#"+rastchatidx);
+        console.log(newchat);
+        console.log(lastchat);
+        lastchat.style.display='none';
+        newchat.style.display='block';
+        rastchatidx=id;
+    }
 }
 
 document.querySelectorAll('#offmenu').forEach((item)=>{
@@ -85,40 +119,46 @@ document.querySelectorAll('#onmenu').forEach((item)=>{
 });
 
 function useradd(){
+    if(idx>10){ showerror(); return;}
     let name = document.getElementById("username");
-    let DMlist = document.getElementsByClassName('rectangle-parent');
     DMlist[0].innerHTML+=`<div class="dm1" id="no">
                                     <div class="select1">
                                     </div>
-                                    <div class="name" id="${idx++}">${name.value}</div>
+                                    <div class="name" id="user${idx++}">${name.value}</div>
                                     <img class="user-frofile-img" alt="" src="source/frofile/discord_blue.png">
                                     <img class="component-12-icon1" alt="" src="source/frofile_icon/Online_no.svg">
                                 </div>`;
+    let listdiv=`<div class="onlineuser">
+                    <img class="user-frofile-img2" alt="" src="source/frofile/discord_blue.png">
+                    <img class="component-12-icon2" alt="" src="source/frofile_icon/Online_list.svg">
+                    <span>${name.value}</span>
+                    <p>온라인</p>
+                </div>`;
+    Onlinelist[0].innerHTML+=listdiv;
+    Alllist[0].innerHTML+=listdiv;
     all();
     document.getElementById("username").value="";
 }
 
+
+function editdiv(classname){
+    let div = document.getElementById('divon');
+    div.setAttribute('id',"divoff");
+    var div2 = document.querySelectorAll('.'+classname);
+    div2[0].setAttribute('id','divon');
+}
 function add(){
     let html = this.innerHTML;
     if(html.indexOf('온라인')>0){
-        let div = document.getElementById('divon');
-        div.setAttribute('id',"divoff");
-        var div2 = document.querySelectorAll('.rectangle-div1');
-        div2[0].setAttribute('id','divon');
+        editdiv('rectangle-div1');
     }else if(html.indexOf('모두')>0){
-        let div = document.getElementById('divon');
-        div.setAttribute('id',"divoff");
-        var div2 = document.querySelectorAll('.allfriends');
-        div2[0].setAttribute('id','divon');
+        editdiv('allfriends');
     }else if(html.indexOf('대기 중')>0){
-        
+        editdiv('request');
     }else if(html.indexOf('차단 목록')>0){
-        
+        editdiv('block');
     }else if(html.indexOf('친구 추가하기')>0){
-        let div = document.getElementById('divon');
-        div.setAttribute('id',"divoff");
-        var div2 = document.querySelectorAll('.addfriends');
-        div2[0].setAttribute('id','divon');
+        editdiv('addfriends');
     }
 
     let a = document.getElementById('onmenu');
